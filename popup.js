33d16ec12589f6b1585e77cf227e0e97ef6c6731
あかあく <105225777+AkaakuHub@users.noreply.github.com>
2023-08-25 21:46:37 +0900

    Delete popup.js

--- popup.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("clickButton").addEventListener("click", function () {
-        browser.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            browser.tabs.sendMessage(tabs[0].id, { action: "extractFunction" });
-        });
-    });
-});
